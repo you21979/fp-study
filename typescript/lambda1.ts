3e20ca832f77ecb00fe3bbc847036f37971b1804
you21979 <you2197901@gmail.com>
2013-02-12 18:59:32 +0900

    add ts

---
+++ typescript/lambda1.ts
@@ -0,0 +1,35 @@
+class Lambda{
+    constructor(public fn:Function){}
+}
+class Thunk{
+    constructor(public val:Function){}
+}
+function lambda(fn:Function):Function{ return new Lambda(fn).fn; }
+function thunk(val:Function):Function{ return new Thunk(val).val; }
+
+class Player{
+    constructor(
+        public charaid:number,
+        public level:number,
+        public hp:number,
+        public name:string
+    ){}
+}
+
+var player:Function =
+                lambda((charaid:number)=>
+                    lambda((level:number)=>
+                        lambda((hp:number)=>
+                            lambda((name:string)=>
+                                thunk(()=>new Player(charaid,level,hp,name))
+                            )
+                        )
+                    )
+                );
+
+// クラス
+console.log(player(1)(10)(120)("hoge")());
+
+// 関数
+console.log(lambda((x)=>lambda((y)=>x * y))(5)(2));
+
